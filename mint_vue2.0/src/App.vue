66aef8d2f10db7d0c6c71c3a0a1eeaea973d3ded
LS--20160803ISH\Administrator <897622361@qq.com>
2017-02-10 17:14:47 +0800

    complete

--- mint_vue2.0/src/App.vue
+++ mint_vue2.0/src/App.vue
@@ -1,31 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <section class="page-demo" v-show="showHome">
+      <div v-for="group in navs">
+        <mt-cell
+           v-html="group.name" @click.native="goRoute(group)">
+
+        </mt-cell>
+      </div>
+    </section>
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      navs:[{name:'Foo',path:'/foo'},{name:'Bar',path:'/bar'},{name:'Toast',path:'/toast'}],
+      showHome:sessionStorage.showHome == undefined?true:eval(sessionStorage.showHome)
     }
+  },
+  methods:{
+      goRoute:function(item){
+          console.log(item);
+          this.showHome = !this.showHome;
+          sessionStorage.showHome = this.showHome;
+          this.$router.push({ path: item.path });
+      }
   }
 }
 </script>
@@ -37,7 +42,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 
 h1, h2 {
