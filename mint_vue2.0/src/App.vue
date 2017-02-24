052a59ccbd6968a8a306483a25f703417d20367f
LS--20160803ISH\Administrator <897622361@qq.com>
2017-02-24 09:42:46 +0800

    day_6

--- mint_vue2.0/src/App.vue
+++ mint_vue2.0/src/App.vue
@@ -29,6 +29,7 @@ export default {
           console.log(item);
           this.showHome = !this.showHome;
           sessionStorage.showHome = this.showHome;
+          console.log(this);
           this.$router.push({ path: item.path });
       }
   }
@@ -42,6 +43,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
   /* margin-top: 60px; */
 }
 
